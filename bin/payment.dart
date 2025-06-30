7a847c3f463667e39c3430bddd33a0a78f4e233a
Faysal Ahmed <techbanglamaxfs@gmail.com>
2025-06-30 18:45:05 +0600

    update 9

--- bin/payment.dart
+++ bin/payment.dart
@@ -8,6 +8,11 @@ main(){
   print("Your Payment Method here.");
   print("Your Payment Method here.");
   print("Your Payment Method here.");
+  print("Your Payment Method here.");
+  print("Your Payment Method here.");
+  print("Your Payment Method here.");
+  print("Your Payment Method here.");
+  print("Your Payment Method here.");
 
   print("Your Payment Method here577.");
   print("Your Payment Method here577.");
